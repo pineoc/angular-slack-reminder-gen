3aca607163b41fdff03c85cc42ad7844966933d3
pineoc <kingsuk92@gmail.com>
2020-09-14 01:14:24 +0900

    added angularjs forms component

---
+++ angularjs/srg-forms/srg-forms.component.js
@@ -0,0 +1,9 @@
+var module = angular.module('srgApp');
+
+// This name is what AngularJS uses to match to the `<srg-forms>` element.
+module.component('srgForms', {
+  templateUrl: 'srg-forms/srg-forms.template.html',
+  controller: function SrgFormController() {
+    
+  }
+});
