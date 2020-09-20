0a5cf5c9fb4b8fcab45ef0f1c06d4d98a354a8e7
pineoc <kingsuk92@gmail.com>
2020-09-20 22:37:35 +0900

    add repeat, once div

--- angularjs/srg-forms/srg-forms.component.js
+++ angularjs/srg-forms/srg-forms.component.js
@@ -4,11 +4,30 @@ var module = angular.module('srgApp', []);
 module.component('srgForms', {
   templateUrl: 'srg-forms/srg-forms.template.html',
   controller: function SrgFormController($scope, $filter) {
+    $scope.checkboxModel = {
+      sunday: false,
+      monday: true,
+      tuesday: false,
+      wednesday: false,
+      thursday: false,
+      friday: false,
+      saturday: false
+    };
     this.generateCommand = function () {
-      console.log($scope);
       var date = $filter('date')($scope.date, 'yyyy-MM-dd');
       var time = $filter('date')($scope.time, 'h:mma');
-      $scope.command = '/remind '+$scope.who+' "'+$scope.what+'" on '+date+' at '+time;
+      console.log($scope);
+      $scope.command = `/remind ${$scope.who} "${$scope.what}"`;
+      if ($scope.reminderType == 'once') {
+        $scope.command += ` on ${date} at ${time}`;
+      } else if ($scope.reminderType == 'repeat') {
+        var days = this.getKeyByValue($scope.checkboxModel, true).join(', ');
+        $scope.command += ` on every ${days}`;
+        $scope.command += ` at ${time}`;
+      }
+    };
+    this.getKeyByValue = function(object, value) {
+      return Object.keys(object).filter(key => object[key] === value);
     };
   }
 });
