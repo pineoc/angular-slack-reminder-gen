d53d2f76ecb5336315d83336cb6a1e9897877f30
pineoc <kingsuk92@gmail.com>
2020-09-24 00:43:10 +0900

    add repeat weekday, weekend day conditions

--- angularjs/srg-forms/srg-forms.component.js
+++ angularjs/srg-forms/srg-forms.component.js
@@ -4,6 +4,10 @@ var module = angular.module('srgApp', []);
 module.component('srgForms', {
   templateUrl: 'srg-forms/srg-forms.template.html',
   controller: function SrgFormController($scope, $filter) {
+    $scope.forms = {
+      date: new Date(),
+      time: new Date(2020, 09, 30, 9, 0, 0)
+    };
     $scope.checkboxModel = {
       sunday: false,
       monday: true,
@@ -14,20 +18,36 @@ module.component('srgForms', {
       saturday: false
     };
     this.generateCommand = function () {
-      var date = $filter('date')($scope.date, 'yyyy-MM-dd');
-      var time = $filter('date')($scope.time, 'h:mma');
-      console.log($scope);
+      var date = $filter('date')($scope.forms.date, 'yyyy-MM-dd');
+      var time = $filter('date')($scope.forms.time, 'h:mma');
       $scope.command = `/remind ${$scope.who} "${$scope.what}"`;
       if ($scope.reminderType == 'once') {
         $scope.command += ` on ${date} at ${time}`;
       } else if ($scope.reminderType == 'repeat') {
-        var days = this.getKeyByValue($scope.checkboxModel, true).join(', ');
-        $scope.command += ` on every ${days}`;
+        $scope.command += ` on ${this.getRepeatDaysString()}`;
         $scope.command += ` at ${time}`;
       }
     };
-    this.getKeyByValue = function(object, value) {
-      return Object.keys(object).filter(key => object[key] === value);
+    this.getRepeatDaysString = function() {
+      function isArrayEqual(array1, array2) {
+        array1 = array1.sort();
+        array2 = array2.sort();
+        return array1.length === array2.length && array1.every((value, index) => value === array2[index]);
+      }
+      function getKeyByValue(object, value) {
+        return Object.keys(object).filter(key => object[key] === value);
+      }
+      var days = getKeyByValue($scope.checkboxModel, true);
+      if (days.length === 7) {
+        return 'everyday';
+      }
+      if (isArrayEqual(days, ['sunday', 'saturday'])) {
+        return 'every weekend';
+      }
+      if (isArrayEqual(days, ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'])) {
+        return 'every weekday';
+      }
+      return 'every ' + days.join(', ');
     };
   }
 });
